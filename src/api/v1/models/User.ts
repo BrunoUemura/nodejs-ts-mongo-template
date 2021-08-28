5caa13ea11a1217d55f41f736eda4e26aed9c898
BrunoUemura <bruno.h.uemura@outlook.com>
2021-08-27 21:10:35 -0300

    Adjust in model

--- src/api/v1/models/User.ts
+++ src/api/v1/models/User.ts
@@ -1,11 +1,27 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  occupation: String,
-  phone: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
   userPhoto: String,
   createdAt: {
     type: Date,
